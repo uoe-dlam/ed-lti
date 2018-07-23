61135b1729c98770874a7e4a880d867b83edabda
sephster <andrew@noexceptions.io>
2018-07-23 15:39:14 +0100

    Reinstate require statement for lti class

--- ed-lti.php
+++ ed-lti.php
@@ -6,6 +6,7 @@ Description: Allows LMSs to connect to site and create blogs
 Author: Richard Lawson (richard.lawson@ed.ac.uk)
 Version: 1.0.0
 */
+require_once "classes/class-ed-lti.php";
 
 new Ed_LTI();
 
