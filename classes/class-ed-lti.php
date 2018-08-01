53bac126d648bc24c18362db80b988c278f104e5
Richard Lawson <richard.lawson@ed.ac.uk>
2018-08-01 16:18:39 +0100

    update documentation

--- classes/class-ed-lti.php
+++ classes/class-ed-lti.php
@@ -241,7 +241,7 @@ class Ed_LTI {
 
 			wp_update_user( $user );
 
-            // set current user back to 0. If not, the user that we created will be used in blog creation and made an admin. We may not want this.
+            // set current user back to 0. If not, this user will be used in blog creation and made an admin. We may not want this.
             wp_set_current_user( 0 );
 		}
 
