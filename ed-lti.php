46f391f77b491dde13691076cac241bd1a9c2b09
Richard Lawson <richard.lawson@ed.ac.uk>
2018-09-13 16:23:12 +0100

    added credits since we borrow a lot of the code for creating/editing/deleting consumer keys from the  LTI Plugin for Wordpress

--- ed-lti.php
+++ ed-lti.php
@@ -5,6 +5,7 @@ Plugin Name: UoE LTI
 Description: Allows LMSs to connect to our website and create blogs.
 Author: DLAM Applications Development Team
 Version: 1.0
+Credits: This plugin was inspired by the IMS Basic Learning Tools Interoperability plugin ( developed by Chuck Severance & Antoni Bertran ). Some of the code used in this plugin is borrowed from that plugin.
 */
 require_once 'classes/class-ed-lti.php';
 
