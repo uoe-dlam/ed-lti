76b42d756805b7cbe91e8dc5169f2917792c48bc
sephster <andrew@noexceptions.io>
2018-10-12 11:39:28 +0100

    Fix minor coding standard issues

--- classes/class-wp-blog-creator.php
+++ classes/class-wp-blog-creator.php
@@ -21,14 +21,14 @@ class WP_Blog_Creator implements Blog_Creator {
 		// We need to make sure our path is not just a slug and contains appropriate slashes.
 		$path = Ed_LTI::turn_slug_into_path( $data['path'] );
 
-        // Unlike NS Cloner, wordpress requires a user to create a blog
-        $default_user_id = wp_get_current_user()->ID;
+		// Unlike NS Cloner, WordPress requires a user to create a blog
+		$default_user_id = wp_get_current_user()->ID;
 		$site_id         = wpmu_create_blog( $data['domain'], $path, $data['title'], $default_user_id );
 		$site_info       = get_blog_details( $site_id );
 
 		if ( $site_info ) {
 			$this->set_blog_template( $site_id );
-            // we don't want the default user to be the site owner, so remove them from the blog we just created
+			// we don't want the default user to be the site owner, so remove them from the blog we just created
 			remove_user_from_blog( $default_user_id, $site_id );
 
 			return $site_id;
